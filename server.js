cb71db9b9b23e186ef829faeb0109ba06dfe55b8
Adrian <adrianbueno095@gmail.com>
2024-01-16 11:34:49 +0100

    add get articles and projects

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
-
+const articles = require("./data/articles.json")
+const projects = require("./data/projects.json")
 const app = express()
 
 app.use(express.json())
@@ -8,14 +9,13 @@ app.get('/', (request, response) => {
   response.send('<h1>Welcome Ironhacker. :) with nodemon</h1>')
 })
 
-app.get('/api/books', (_, response) => {
-  const books = [
-    { title: 'Rick&Morty', pages: 35 },
-    { title: 'Solo Leveling', pages: 150 },
-    { title: 'Arslan', pages: 150 },
-  ]
-
-  response.json(books)
+app.get('/api/projects', (_, response) => {
+  response.json(projects)
 })
+
+app.get('/api/articles', (_, response) => {
+  response.json(articles)
+})
+
 
 app.listen(3000, () => console.log('My first app listening on port 3000!'))
