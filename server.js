a3bfe103a0e13c7b26b9db016ac8cd6eda6a141a
Adrian <adrianbueno095@gmail.com>
2024-01-16 11:37:44 +0100

    add a 404 route

--- server.js
+++ server.js
@@ -17,5 +17,9 @@ app.get('/api/articles', (_, response) => {
   response.json(articles)
 })
 
+app.use((req, res, next) => {
+  res.status(404).sendFile(__dirname + "views/not-found.html")
+})
+
 
 app.listen(3000, () => console.log('My first app listening on port 3000!'))
